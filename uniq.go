14f14a3e9c5a94e9a5e7482f19e99b8b57443e4a
Anschel Schaffer-Cohen <anschelsc@gmail.com>
2012-04-08 23:25:18 -0400

    Not utterly broken, still untested.

--- uniq.go
+++ uniq.go
@@ -3,11 +3,13 @@ package main
 var uniqs = make(chan uint64)
 
 func init() {
-	i := uint64(0)
-	for {
-		uniqs <- i
-		i++
-	}
+	go func() {
+		i := uint64(0)
+		for {
+			uniqs <- i
+			i++
+		}
+	}()
 }
 
 func uniq() uint64 {
